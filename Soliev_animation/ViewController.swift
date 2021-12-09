663d8e8671c710492fd93e3e67ddc13ea444b523
Student <student@402-06.local>
2021-12-09 13:54:23 +0300

    already done

---
+++ Soliev_animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Soliev_animation
+//
+//  Created by Student on 09.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
